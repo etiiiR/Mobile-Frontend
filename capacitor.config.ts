ca7c78c88698cd72f54cd4d619e649fd821b0956
Etienne Roulet <etienne.roulet@outlook.com>
2023-02-23 21:28:36 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'CDL1 Sensor Activities',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
