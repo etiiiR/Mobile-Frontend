74a854e07551b4e2fe482e1f6379eec474c00be5
Etienne Roulet <etienne.roulet@outlook.com>
2023-02-24 00:58:22 +0100

    Device Orientation reactive
    Device Orientation reactive

--- src/views/Tab3Page.vue
+++ src/views/Tab3Page.vue
@@ -11,13 +11,18 @@
           <ion-title size="large">Tab 3</ion-title>
         </ion-toolbar>
       </ion-header>
-
-      <ExploreContainer name="Tab 3 page" />
+      <note class="mb-2"> Device Orientation: </note>
+      <pre lang="json">{{ text }}</pre>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
+import { useDeviceOrientation } from '@vueuse/core'
+import { reactive, computed } from "vue";
+
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
 import ExploreContainer from '@/components/ExploreContainer.vue';
+const orientation = reactive(useDeviceOrientation());
+const text = computed(() => JSON.stringify(orientation, null, 2));
 </script>
