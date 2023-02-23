1c5e546a25f29d35fd46e59c70c5b98e47175522
Etienne Roulet <etienne.roulet@outlook.com>
2023-02-23 23:51:14 +0100

    Update Tab1Page.vue

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -31,10 +31,7 @@ import ExploreContainer from "@/components/ExploreContainer.vue";
 const acceleration = ref({ x: 0, y: 0, z: 0 });
 const accelerationIncludingGravity = ref({ x: 0, y: 0, z: 0 });
 const orientation = ref({ alpha: 0, beta: 0, gamma: 0 });
-const ButtonText = ref('Stop Measure')
-onMounted(async () => {
-  checkpermission()
-})
+const ButtonText = ref('Start Measure')
 
 const checkpermission = async () => {
   console.log('clicked')
