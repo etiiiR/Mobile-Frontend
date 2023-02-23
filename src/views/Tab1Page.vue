2279c60c0b62ba3f6ecb8e8671c0514e5eda36cd
Etienne Roulet <etienne.roulet@outlook.com>
2023-02-23 22:14:55 +0100

    Update Tab1Page.vue

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -13,12 +13,23 @@
       </ion-header>
 
       <ExploreContainer name="Tab 1 page" />
+      <p>{{ motion.x }}{{ motion.y }}{{ motion.z }}</p>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import { Motion } from '@capacitor/motion';
+
 
 import ExploreContainer from '@/components/ExploreContainer.vue';
+const motion = ref({x: 0, y: 0, z: 0})
+
+Motion.addListener('accel', (accel) => {
+  console.log('accel', accel);
+  motion.value = accel
+});
+
 </script>
