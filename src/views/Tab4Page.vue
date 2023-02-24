c48a919c95d110b8b7d38803aeeb0f571703a9a1
Etienne Roulet <etienne.roulet@outlook.com>
2023-02-24 01:03:07 +0100

    tabs
    tabs

---
+++ src/views/Tab4Page.vue
@@ -0,0 +1,28 @@
+<template>
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-title>Tab 4</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Tab 4</ion-title>
+        </ion-toolbar>
+      </ion-header>
+      <note class="mb-2"> Device Orientation: </note>
+      <pre lang="json">{{ text }}</pre>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { useGeolocation } from '@vueuse/core'
+import { reactive, computed } from "vue";
+
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import ExploreContainer from '@/components/ExploreContainer.vue';
+const geolocation = reactive(useGeolocation());
+const text = computed(() => JSON.stringify(geolocation, null, 2));
+</script>
--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -5,17 +5,21 @@
       <ion-tab-bar slot="bottom">
         <ion-tab-button tab="tab1" href="/tabs/tab1">
           <ion-icon aria-hidden="true" :icon="logoElectron" />
-          <ion-label>Tab 1</ion-label>
+          <ion-label>Own Implementation</ion-label>
         </ion-tab-button>
 
         <ion-tab-button tab="tab2" href="/tabs/tab2">
           <ion-icon aria-hidden="true" :icon="logoApple" />
-          <ion-label>Tab 2</ion-label>
+          <ion-label>Accelerator</ion-label>
         </ion-tab-button>
 
         <ion-tab-button tab="tab3" href="/tabs/tab3">
           <ion-icon aria-hidden="true" :icon="watch" />
-          <ion-label>Tab 3</ion-label>
+          <ion-label>Orientation</ion-label>
+        </ion-tab-button>
+        <ion-tab-button tab="tab4" href="/tabs/tab3">
+          <ion-icon aria-hidden="true" :icon="watch" />
+          <ion-label>Geolocation</ion-label>
         </ion-tab-button>
       </ion-tab-bar>
     </ion-tabs>
