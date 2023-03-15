4b57296797d91a7bbb0d72a5adcd4646b5181d70
Etienne Roulet <etienne.roulet@outlook.com>
2023-03-15 19:30:53 +0100

    Update Tab1Page.vue

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -11,6 +11,7 @@
           <ion-title size="large">Tab 1</ion-title>
         </ion-toolbar>
       </ion-header>
+      <ion-button @click="checkpermission" >Allow Motion Permission</ion-button>
       <note class="mb-2"> Device Motion: </note>
       <pre lang="json">{{ textMotion }}</pre>
       <note class="mb-2"> Device Orientation: </note>
@@ -48,6 +49,7 @@ import { Motion } from "@capacitor/motion";
 import { useDeviceMotion, usePermission, useDeviceOrientation } from "@vueuse/core";
 import { reactive, computed, onMounted } from "vue";
 import { useGeolocation } from "@vueuse/core";
+import { Dialog } from '@capacitor/dialog';
 
 const ButtonText = ref("Start Measure");
 const permission = ref(false);
@@ -91,8 +93,7 @@ const predictData = async () => {
 const checkpermission = async () => {
   try {
     await DeviceMotionEvent.requestPermission();
-    await DeviceOrientationEvent.requestPermission();
-    permission.value = true;
+    //await DeviceOrientationEvent.requestPermission();
   } catch (e) {
     // Handle error
     presentAlert(e.message);
@@ -113,7 +114,8 @@ const presentAlert = async (m: string) => {
 };
 
 onMounted(async () => {
-  debugger
   await checkpermission();
 });
+
+
 </script>
